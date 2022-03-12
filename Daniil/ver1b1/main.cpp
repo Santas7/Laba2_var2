9eda3ecf5c4d55c5647d0d7e4753fb92a7e34dbb
the80hz <64586195+the80hz@users.noreply.github.com>
2022-03-12 23:56:29 +0400

    refactor project

--- Daniil/ver1b1/main.cpp
+++ Daniil/ver1b1/main.cpp
@@ -2,51 +2,69 @@
 
 class interval{
 private:
-    double a{};
-    double b{};
+    double left, right;
 
 public:
-    void setA(int value){a = value;}
-    void setB(int value){b = value;}
-    double getA() const{return a;}
-    double getB() const{return b;}
+    void setLeft(double value){left = value;}
+    void setRight(double value){right = value;}
+    double getLeft() const{return left;}
+    double getRight() const{return right;}
 
     void print() const{
-        printf("(%d;%d)", a, b);
+        printf("(%g;%g)", left, right);
     }
 
-    explicit interval(int a = 0, int b = 0){
-        setA(a);
-        setB(b);
+    void enterLeft(){
+        double value;
+        printf("Left: ");
+        scanf("%lf", &value);
+        left = value;
+    }
+
+    void enterRight(){
+        double value;
+        while(true){
+            printf("Right: ");
+            scanf("%lf", &value);
+            if(value < left){
+                printf("\x1B[31mThe right number must be greater than the left one!\n\033[0m");
+            }
+            else{
+                right = value;
+                break;
+            }
+        }
     }
 };
 
-int main() {
-    interval i1;
-    interval i2;
+interval intersection(interval i1, interval i2){
+    interval cross{};
+    cross.setLeft(0);
+    cross.setRight(0);
 
-    int a1, b1, a2, b2;
+    
 
+    return cross;
+}
+
+int main(){
+    // the first interval
+    interval i1{};
     printf("Enter the first interval:\n");
-    printf("a = ");
-    scanf("%d", &a1);
-    printf("b = ");
-    scanf("%d", &b1);
-
-    i1.setA(a1);
-    i1.setB(b1);
-
-    printf("Enter the second interval:\n");
-    printf("a = ");
-    scanf("%d", &a2);
-    printf("b = ");
-    scanf("%d", &b2);
-
-    i2.setA(a2);
-    i2.setB(b2);
-
+    i1.enterLeft();
+    i1.enterRight();
+    printf("The first interval: ");
     i1.print();
+
+    // the second interval
+    interval i2{};
+    printf("\n\nEnter the second interval:\n");
+    i2.enterLeft();
+    i2.enterRight();
+    printf("The second interval: ");
     i2.print();
+
+
 
     return 0;
 }
